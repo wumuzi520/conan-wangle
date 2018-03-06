becb8bdfce4f6c64f92d8c9750891e8134c02e17
ZhuSenlin <wumuzi520@126.com>
2018-03-07 02:31:10 +0800

    bugfix

--- FindWangle.cmake
+++ FindWangle.cmake
@@ -1,18 +1,18 @@
 #
 # Find WANGLE
 #
-#  WANGLE_INCLUDE_DIR - where to find wangle/service/Service.h, etc.
-#  WANGLE_LIBRARY     - List of libraries when using libwangle.
+#  WANGLE_INCLUDE_DIRS - where to find wangle/service/Service.h, etc.
+#  WANGLE_LIBRARIES     - List of libraries when using libwangle.
 #  WANGLE_FOUND       - True if libwangle found.
 
-FIND_PATH(WANGLE_INCLUDE_DIR wangle/service/Service.h)
+FIND_PATH(WANGLE_INCLUDE_DIRS wangle/service/Service.h)
 
-FIND_LIBRARY(WANGLE_LIBRARY NAMES wangle)
+FIND_LIBRARY(WANGLE_LIBRARIES NAMES wangle)
 
 # handle the QUIETLY and REQUIRED arguments and set WANGLE_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(WANGLE DEFAULT_MSG WANGLE_LIBRARY WANGLE_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(WANGLE DEFAULT_MSG WANGLE_LIBRARIES WANGLE_INCLUDE_DIRS)
 
-MARK_AS_ADVANCED(WANGLE_LIBRARY WANGLE_INCLUDE_DIR)
+MARK_AS_ADVANCED(WANGLE_LIBRARIES WANGLE_INCLUDE_DIRS)
 
